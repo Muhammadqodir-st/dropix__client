db88433a323f9a6ccf3105e190134b27aaecdce1
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-08-26 15:22:38 +0500

    login and sing up page responsive

--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -12,13 +12,13 @@ export default function SingUp() {
     return (
         <div className='flex items-center justify-center'>
             <video className='w-full h-screen object-cover' src={video} autoPlay loop muted></video>
-            <div className='p-8 absolute bg-white/10 backdrop-blur-md shadow-lg rounded-2xl flex flex-col gap-7 border border-white/20'>
+            <div className='p-8 absolute bg-white/10 backdrop-blur-md shadow-lg rounded-2xl flex flex-col gap-7 border border-white/20 max-[600px]:max-w-[80%] max-[400px]:gap-4 max-[400px]:p-5'>
                 <div className='w-fit p-1 bg-black rounded-full flex items-center justify-center space-x-2'>
                     <Link to={"/login"}>
-                        <button className={location.pathname === "/login" ? "py-3 px-8 text-white bg-white/10 rounded-full cursor-pointer border border-white/11" : "py-3 px-8 text-gray-600 rounded-full cursor-pointer"}>Login</button>
+                        <button className={location.pathname === "/login" ? "py-3 px-8 text-white bg-white/10 rounded-full cursor-pointer border border-white/11 max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm" : "py-3 px-8 text-gray-600 rounded-full cursor-pointer max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm"}>Login</button>
                     </Link>
                     <Link to={"/signup"}>
-                        <button className={location.pathname === "/signup" ? "py-3 px-8 text-white bg-white/10 rounded-full cursor-pointer border border-white/11" : "py-3 px-8 text-gray-600 rounded-full cursor-pointer"}>Sign up</button>
+                        <button className={location.pathname === "/signup" ? "py-3 px-8 text-white bg-white/10 rounded-full cursor-pointer border border-white/11 max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm" : "py-3 px-8 text-gray-600 rounded-full cursor-pointer max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm"}>Sign up</button>
                     </Link>
                 </div>
                 <Link to={"/"}>
@@ -27,14 +27,14 @@ export default function SingUp() {
                     </div>
                 </Link>
 
-                <h1 className='text-4xl text-white w-100'>Create an account</h1>
+                <h1 className='text-4xl text-white w-100 max-[500px]:w-auto max-[500px]:text-3xl max-[400px]:text-2xl'>Create an account</h1>
                 <form className='w-full flex flex-col gap-4'>
                     <label className='w-full flex bg-white/10 p-3 items-center justify-between gap-2 rounded-lg border border-white/20'>
-                        <User className='text-white cursor-pointer' size={22} />
+                        <User className='text-white/10 cursor-pointer max-[400px]:hidden' size={22} />
                         <input className='flex-1 outline-0 text-white' type="text" placeholder='Name' />
                     </label>
                     <label className='w-full flex bg-white/10 p-3 items-center justify-between gap-2 rounded-lg border border-white/20'>
-                        <Mail className="text-white cursor-pointer" size={22} />
+                        <Mail className="text-white/10 cursor-pointer max-[400px]:hidden" size={22} />
                         <input className='flex-1 outline-0 text-white' type="email" placeholder='Email' />
                     </label>
                     <button type='button' className='w-full bg-white py-3 rounded-lg cursor-pointer text-black'>Create an account</button>
@@ -44,11 +44,11 @@ export default function SingUp() {
                     <p className='text-lg font-bold text-white/10'>OR</p>
                     <div className='flex-1 h-[3px] bg-white/10'></div>
                 </div>
-                <div className='flex items-center justify-center gap-5'>
-                    <button className='flex-1 bg-white/10 rounded-lg py-3 flex items-center justify-center border border-white/20 cursor-pointer'>
+                <div className='flex items-center justify-center gap-5 max-[400px]:flex-col max-[400px]:gap-3'>
+                    <button className='w-full flex-1 bg-white/10 rounded-lg py-3 flex items-center justify-center border border-white/20 cursor-pointer max-[400px]:hidden'>
                         <img className='w-7' src={Google} alt="" />
                     </button>
-                    <button className='flex-1 bg-white/10 rounded-lg py-3 flex items-center justify-center border border-white/20 cursor-pointer'>
+                    <button className='w-full flex-1 bg-white/10 rounded-lg py-3 flex items-center justify-center border border-white/20 cursor-pointer max-[400px]:py-2'>
                         <img className='w-7' src={Email} alt="" />
                     </button>
                 </div>
