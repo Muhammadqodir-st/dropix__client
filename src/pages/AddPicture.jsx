c7fd47007c2208b6758cf28716c57b47f360c50c
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-09-03 17:10:31 +0500

    add picture page upload

--- src/pages/AddPicture.jsx
+++ src/pages/AddPicture.jsx
@@ -1,7 +1,21 @@
-export default function AddPicture(){
-    return(
+export default function AddPicture() {
+    return (
         <div className="w-full px-4">
-            <h1>add picture</h1>
+            <div className="flex gap-5">
+                <div>
+                    <textarea className="w-130 h-100 border-2 border-dashed border-gray-500 rounded-lg outline-0 cursor-pointer" disabled></textarea>
+                </div>
+                <div className="flex flex-col gap-3">
+                    <label className="flex flex-col">
+                        <p className="text-md text-gray-700">Title</p>
+                        <input className="w-90 p-2 border border-gray-500 rounded-md outline-0 text-gray-700" type="text" />
+                    </label>
+                    <label className="flex flex-col">
+                        <p className="text-md text-gray-700">Description</p>
+                        <textarea className="w-90 p-2 border border-gray-500 rounded-md outline-0 text-gray-700"></textarea>
+                    </label>
+                </div>
+            </div>
         </div>
     )
 }
