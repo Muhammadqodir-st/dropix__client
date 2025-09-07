754a9420b75a0119079e6e2009004ab3bba83500
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-09-07 11:32:39 +0500

    sign up update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,58 +1,33 @@
-// icon and video
-import video from '../assets/bg.mp4'
-import { User } from 'lucide-react';
-import { Mail } from 'lucide-react';
-import { X } from 'lucide-react';
-import Google from '../assets/google.png'
-import Email from '../assets/email.png'
-// react router dom
-import { Link } from 'react-router-dom';
+// assets
+import bg from '../assets/bg.png'
+import logo from '../assets/login-logo.svg'
+
+// react router dom 
+import { Link } from 'react-router-dom'
 
 export default function Login() {
     return (
-        <div className='flex items-center justify-center'>
-            <video className='w-full h-screen object-cover' src={video} autoPlay loop muted></video>
-            <div className='p-8 absolute bg-white/10 backdrop-blur-md shadow-lg rounded-2xl flex flex-col gap-7 border border-white/20 max-[600px]:max-w-[80%] max-[400px]:gap-4 max-[400px]:p-5'>
-                <div className='w-fit p-1 bg-black rounded-full flex items-center justify-center space-x-2'>
-                    <Link to={"/login"}>
-                        <button className={location.pathname === "/login" ? "py-3 px-8 text-white bg-white/10 rounded-full cursor-pointer border border-white/11 max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm" : "py-3 px-8 text-gray-600 rounded-full cursor-pointer max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm"}>Login</button>
-                    </Link>
-                    <Link to={"/signup"}>
-                        <button className={location.pathname === "/signup" ? "py-3 px-8 text-white bg-white/10 rounded-full cursor-pointer border border-white/11 max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm" : "py-3 px-8 text-gray-600 rounded-full cursor-pointer max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm"}>Sign up</button>
-                    </Link>
+        <div className='w-full h-screen flex items-center justify-center bg-[#f2f2f2]'>
+            <div className='w-280 h-165 bg-white rounded-2xl overflow-hidden flex items-center gap-3'>
+                <div className='w-[50%] h-full'>
+                    <img className='w-full h-full object-cover' src={bg} alt="" />
                 </div>
-                <Link to={"/"}>
-                    <div className='w-10 h-10 bg-white/10 rounded-full flex items-center justify-center cursor-pointer absolute top-5 right-5'>
-                        <X className='text-white' size={22} />
-                    </div>
-                </Link>
-
-                <h1 className='text-4xl text-white w-100 max-[500px]:w-auto max-[500px]:text-3xl max-[400px]:text-2xl'>Welcome back</h1>
-                <form className='w-full flex flex-col gap-4'>
-                    <label className='w-full flex bg-white/10 p-3 items-center justify-between gap-2 rounded-lg border border-white/20'>
-                        <User className='text-white/10 cursor-pointer max-[400px]:hidden' size={22} />
-                        <input className='flex-1 outline-0 text-white' type="text" placeholder='Name' />
-                    </label>
-                    <label className='w-full flex bg-white/10 p-3 items-center justify-between gap-2 rounded-lg border border-white/20'>
-                        <Mail className='text-white/10 cursor-pointer max-[400px]:hidden' size={22} />
-                        <input className='flex-1 outline-0 text-white' type="email" placeholder='Email' />
-                    </label>
-                    <button type='button' className='w-full bg-white py-3 rounded-lg cursor-pointer text-black'>Login</button>
-                </form>
-                <div className='w-full flex items-center justify-center gap-3'>
-                    <div className='flex-1 h-[3px] bg-white/10'></div>
-                    <p className='text-lg font-bold text-white/10'>OR</p>
-                    <div className='flex-1 h-[3px] bg-white/10'></div>
+                <div className='w-[50%] h-full flex items-center justify-center'>
+                    <form className='w-[80%] flex flex-col gap-5'>
+                        <img className='w-43' src={logo} alt="" />
+                        <p className='text-3xl font-bold'>Login to account</p>
+                        <p className='text-lg text-gray-700'>Don't have an account yet? <Link to={''} className='text-indigo-700'>Sign up</Link></p>
+                        <label className='flex flex-col '>
+                            <p className='text-gray-700'>Name</p>
+                            <input className='p-2 rounded-lg border border-gray-500 outline-indigo-500' type="text" />
+                        </label>
+                        <label className='flex flex-col '>
+                            <p className='text-gray-700'>Email</p>
+                            <input className='p-2 rounded-lg border border-gray-500 outline-indigo-500' type="text" />
+                        </label>
+                        <button className='w-full py-2 bg-indigo-700 text-white rounded-lg'>Login</button>
+                    </form>
                 </div>
-                <div className='flex items-center justify-center gap-5 max-[400px]:flex-col max-[400px]:gap-3'>
-                    <button className='w-full flex-1 bg-white/10 rounded-lg py-3 flex items-center justify-center border border-white/20 cursor-pointer max-[400px]:hidden'>
-                        <img className='w-7' src={Google} alt="" />
-                    </button>
-                    <button className='w-full flex-1 bg-white/10 rounded-lg py-3 flex items-center justify-center border border-white/20 cursor-pointer max-[400px]:py-2'>
-                        <img className='w-7' src={Email} alt="" />
-                    </button>
-                </div>
-                <p className='text-sm text-center text-gray-600'>By login an account you agree to our Terms & Service</p>
             </div>
         </div>
     )
--- src/pages/SingUp.jsx
+++ src/pages/SingUp.jsx
@@ -1,58 +1,48 @@
-// icon and video
-import video from '../assets/bg.mp4'
-import { User } from 'lucide-react';
-import { Mail } from 'lucide-react';
-import { X } from 'lucide-react';
-import Google from '../assets/google.png'
-import Email from '../assets/email.png'
-// react router dom
-import { Link } from 'react-router-dom';
+// assets
+import bg from '../assets/bg.png'
+import logo from '../assets/login-logo.svg'
+
+// react router dom 
+import { Link } from 'react-router-dom'
+
+// lucide
+import { ChevronLeft } from 'lucide-react'
+
 
 export default function SingUp() {
     return (
-        <div className='flex items-center justify-center'>
-            <video className='w-full h-screen object-cover' src={video} autoPlay loop muted></video>
-            <div className='p-8 absolute bg-white/10 backdrop-blur-md shadow-lg rounded-2xl flex flex-col gap-7 border border-white/20 max-[600px]:max-w-[80%] max-[400px]:gap-4 max-[400px]:p-5'>
-                <div className='w-fit p-1 bg-black rounded-full flex items-center justify-center space-x-2'>
-                    <Link to={"/login"}>
-                        <button className={location.pathname === "/login" ? "py-3 px-8 text-white bg-white/10 rounded-full cursor-pointer border border-white/11 max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm" : "py-3 px-8 text-gray-600 rounded-full cursor-pointer max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm"}>Login</button>
+        <div className='w-full h-screen flex items-center justify-center bg-[#f2f2f2]'>
+            <div className='w-280 h-165 bg-white rounded-2xl overflow-hidden flex items-center gap-3 max-[1150px]:w-[90%] max-[750px]:h-auto max-[750px]:py-10'>
+                <div className='w-[50%] h-full relative max-[750px]:hidden'>
+                    <img className='w-full h-full object-cover' src={bg} alt="" />
+                    <Link to={'/'}>
+                        <div className='p-2 absolute bg-gray-100 z-100 top-2 left-2 rounded-lg cursor-pointer'>
+                            <ChevronLeft className='text-gray-700' size={25} />
+                        </div>
                     </Link>
-                    <Link to={"/signup"}>
-                        <button className={location.pathname === "/signup" ? "py-3 px-8 text-white bg-white/10 rounded-full cursor-pointer border border-white/11 max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm" : "py-3 px-8 text-gray-600 rounded-full cursor-pointer max-[450px]:py-2 max-[450px]:px-6 max-[400px]:text-sm"}>Sign up</button>
-                    </Link>
                 </div>
-                <Link to={"/"}>
-                    <div className='w-10 h-10 bg-white/10 rounded-full flex items-center justify-center cursor-pointer absolute top-5 right-5'>
-                        <X className='text-white' size={22} />
-                    </div>
-                </Link>
-
-                <h1 className='text-4xl text-white w-100 max-[500px]:w-auto max-[500px]:text-3xl max-[400px]:text-2xl'>Create an account</h1>
-                <form className='w-full flex flex-col gap-4'>
-                    <label className='w-full flex bg-white/10 p-3 items-center justify-between gap-2 rounded-lg border border-white/20'>
-                        <User className='text-white/10 cursor-pointer max-[400px]:hidden' size={22} />
-                        <input className='flex-1 outline-0 text-white' type="text" placeholder='Name' />
-                    </label>
-                    <label className='w-full flex bg-white/10 p-3 items-center justify-between gap-2 rounded-lg border border-white/20'>
-                        <Mail className="text-white/10 cursor-pointer max-[400px]:hidden" size={22} />
-                        <input className='flex-1 outline-0 text-white' type="email" placeholder='Email' />
-                    </label>
-                    <button type='button' className='w-full bg-white py-3 rounded-lg cursor-pointer text-black'>Create an account</button>
-                </form>
-                <div className='w-full flex items-center justify-center gap-3'>
-                    <div className='flex-1 h-[3px] bg-white/10'></div>
-                    <p className='text-lg font-bold text-white/10'>OR</p>
-                    <div className='flex-1 h-[3px] bg-white/10'></div>
+                <div className='w-[50%] h-full flex items-center justify-center max-[750px]:w-full'>
+                    <form className='w-[80%] flex flex-col gap-5' autoComplete='off'>
+                        <Link className='w-auto cursor-pointer' to={'/'}>
+                            <img className='w-43' src={logo} alt="" />
+                        </Link>
+                        <p className='text-3xl font-bold'>Sign up</p>
+                        <p className='text-lg text-gray-700'>Already have an account? <Link to={'/auth/login'} className='text-indigo-700'>Login</Link></p>
+                        <label className='flex flex-col '>
+                            <p className='text-gray-700'>Username</p>
+                            <input className='p-2 rounded-lg border border-gray-500 outline-indigo-500' type="text" name='new-username' required autoFocus autoComplete='off' />
+                        </label>
+                        <label className='flex flex-col '>
+                            <p className='text-gray-700'>Email</p>
+                            <input className='p-2 rounded-lg border border-gray-500 outline-indigo-500' type="email" name='new-email' required autoComplete='off' />
+                        </label>
+                        <label className='flex flex-col '>
+                            <p className='text-gray-700'>Password</p>
+                            <input className='p-2 rounded-lg border border-gray-500 outline-indigo-500' type="password" name='new-password' required autoComplete='new-password' />
+                        </label>
+                        <button className='w-full py-2 bg-indigo-700 text-white rounded-lg cursor-pointer'>Sign up</button>
+                    </form>
                 </div>
-                <div className='flex items-center justify-center gap-5 max-[400px]:flex-col max-[400px]:gap-3'>
-                    <button className='w-full flex-1 bg-white/10 rounded-lg py-3 flex items-center justify-center border border-white/20 cursor-pointer max-[400px]:hidden'>
-                        <img className='w-7' src={Google} alt="" />
-                    </button>
-                    <button className='w-full flex-1 bg-white/10 rounded-lg py-3 flex items-center justify-center border border-white/20 cursor-pointer max-[400px]:py-2'>
-                        <img className='w-7' src={Email} alt="" />
-                    </button>
-                </div>
-                <p className='text-sm text-center text-gray-600'>By creating an account you agree to our Terms & Service</p>
             </div>
         </div>
     )
