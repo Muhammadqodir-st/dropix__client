af664ca3d24cbbc34bb2eb59018be603caca0510
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-09-09 07:25:13 +0500

    addPicture page upload

--- src/pages/AddPicture.jsx
+++ src/pages/AddPicture.jsx
@@ -1,18 +1,47 @@
+// react router dom
+import { useState } from "react"
+
+// lucide icons
+import { ImagePlus } from "lucide-react";
+
+
+
 export default function AddPicture() {
+    // useStates
+    const [image, setImage] = useState(null);
+
+    // functions
+    const handleImgUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImage(URL.createObjectURL(file));
+        }
+    }
+
     return (
         <div className="w-full px-4">
-            <div className="flex gap-5">
-                <div>
-                    <textarea className="w-130 h-100 border-2 border-dashed border-gray-500 rounded-lg outline-0 cursor-pointer" disabled></textarea>
-                </div>
+            <div className="w-full flex gap-3">
+                {/* image upload */}
+                <label className={`w-[450px] h-[500px] rounded-2xl flex items-center justify-center cursor-pointer bg-gray-50 hover:bg-gray-100" htmlFor="imgInput ${image ? 'border-none' : 'border-2 border-dashed border-[#bababa]'}`}>
+                    {image ? (
+                        <img className="w-full h-full object-cover rounded-2xl" src={image} alt="" />
+                    ) : (
+                        <div className="flex flex-col items-center ">
+                            <ImagePlus className="text-gray-500" size={25} />
+                            <span className="text-gray-500">Click or drag image here</span>
+                        </div>
+                    )}
+                    <input onChange={handleImgUpload} className="hidden" type="file" id="imgInput" accept="image/*" />
+                </label>
+                {/* post data */}
                 <div className="flex flex-col gap-3">
-                    <label className="flex flex-col">
-                        <p className="text-md text-gray-700">Title</p>
-                        <input className="w-90 p-2 border border-gray-500 rounded-md outline-0 text-gray-700" type="text" />
+                    <label className="">
+                        <p className="text-gray-500">Title</p>
+                        <input className="w-100 py-3 px-2 border border-[#bababa] rounded-lg bg-gray-50" type="text" placeholder="title here" disabled={!image} />
                     </label>
-                    <label className="flex flex-col">
-                        <p className="text-md text-gray-700">Description</p>
-                        <textarea className="w-90 p-2 border border-gray-500 rounded-md outline-0 text-gray-700"></textarea>
+                    <label className="">
+                        <p className="text-gray-500">Discription</p>
+                        <textarea className="w-100 h-30 py-2 px-2 border border-[#bababa] rounded-lg bg-gray-50" placeholder="discription" disabled={!image}></textarea>
                     </label>
                 </div>
             </div>
