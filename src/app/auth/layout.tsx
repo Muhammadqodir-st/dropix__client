1d0c90ce8d120ba5845abeed93f08c796ab86614
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-11-04 18:04:45 +0500

    fix

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,10 +6,10 @@ import Link from "next/link"
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="w-full h-full flex bg-[#05050D] text-white">
-            <Image className="w-150 h-screen object-cover hidden md:block" src="/auth/back.jpg" alt="backroundImage" width={1000} height={400} priority unoptimized />
-            <div className="py-30 px-10 flex flex-col gap-15">
+            <Image className="w-155 h-screen object-cover hidden md:block" src="/auth/back.jpg" alt="backroundImage" width={1000} height={400} priority unoptimized />
+            <div className="py-10 px-10 flex flex-col gap-15">
                 <Link href={'/'}>
-                    <Image className="w-65" src="/auth/logo.svg" alt="logo" width={100} height={100} />
+                    <Image className="w-20" src="/assets/title.svg" alt="logo" width={100} height={100} />
                 </Link>
 
                 {children}
